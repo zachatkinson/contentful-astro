c2faff97ce07bef3007bacb11d3951699cce7a22
zachatkinson <zach.atkinson85@gmail.com>
2025-01-09 14:44:32 -0500

    hero typescript updates and code cleanup

--- src/lib/componentHero.ts
+++ src/lib/componentHero.ts
@@ -1,9 +1,16 @@
 import type {EntryFieldTypes} from "contentful";
 
+
 export interface ComponentHero{
     contentTypeId: "componentHero",
     fields: {
         title: EntryFieldTypes.Text,
-        background: EntryFieldTypes.AssetLink
+        content: EntryFieldTypes.RichText,
+        slug: EntryFieldTypes.Text,
+        buttonUrl: EntryFieldTypes.Text,
+        buttonText: EntryFieldTypes.Text,
+        desktopBackground: EntryFieldTypes.AssetLink,
+        mobileBackground: EntryFieldTypes.AssetLink,
+
     }
 }
