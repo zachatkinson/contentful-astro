17b8056c60fc5844525897795ab24a210a921b75
Zach Atkinson <zach.atkinson85@gmail.com>
2025-03-12 19:55:32 -0400

    added useSlidesAtlas and useEffectsAtlas to our Kinetic Slider props

--- src/components/KineticSlider3/types.ts
+++ src/components/KineticSlider3/types.ts
@@ -69,6 +69,9 @@ export interface KineticSliderProps {
     // Atlas configuration
     slidesAtlas?: string;
     effectsAtlas?: string;
+
+    useSlidesAtlas?: boolean;
+    useEffectsAtlas?: boolean;
 }
 
 /**
